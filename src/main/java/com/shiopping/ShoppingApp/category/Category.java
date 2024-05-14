45a5194ae5bcf69cdf32620c8fa5a9fd609a25ac
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-14 23:01:14 +0200

    Add category table

---
+++ src/main/java/com/shiopping/ShoppingApp/category/Category.java
@@ -0,0 +1,24 @@
+package com.shiopping.ShoppingApp.category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String categoryName;
+
+}
