8d3e282d23b43de9d73e9aedd11a1baf9c626573
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-15 23:03:48 +0200

    Add category repository

---
+++ src/main/java/com/shiopping/ShoppingApp/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.shiopping.ShoppingApp.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
