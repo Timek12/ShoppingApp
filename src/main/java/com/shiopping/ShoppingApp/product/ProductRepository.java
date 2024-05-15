0dae7b9bd33693c767af326a3cc3a51523d92eed
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-15 23:03:59 +0200

    Add product repository

---
+++ src/main/java/com/shiopping/ShoppingApp/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.shiopping.ShoppingApp.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
