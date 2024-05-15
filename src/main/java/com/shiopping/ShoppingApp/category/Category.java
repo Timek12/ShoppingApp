27cdfa32459e86c479d09a7890d627e4cacfd0be
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-15 23:03:28 +0200

    Add constructor

--- src/main/java/com/shiopping/ShoppingApp/category/Category.java
+++ src/main/java/com/shiopping/ShoppingApp/category/Category.java
@@ -21,4 +21,7 @@ public class Category {
     private Integer id;
     private String categoryName;
 
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
