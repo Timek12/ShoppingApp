bb07f4d6b4994f3739938703d8b5731aae3ceb1b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-23 22:06:01 +0200

    Add findByUser method

--- src/main/java/com/shiopping/ShoppingApp/usershoppinglist/UserShoppingListRepository.java
+++ src/main/java/com/shiopping/ShoppingApp/usershoppinglist/UserShoppingListRepository.java
@@ -1,7 +1,11 @@
 package com.shiopping.ShoppingApp.usershoppinglist;
 
+import com.shiopping.ShoppingApp.shoppinglist.ShoppingList;
+import com.shiopping.ShoppingApp.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserShoppingListRepository extends JpaRepository<UserShoppingList, Integer> {
+import java.util.List;
 
+public interface UserShoppingListRepository extends JpaRepository<UserShoppingList, Integer> {
+    List<UserShoppingList> findByUser(User user);
 }
