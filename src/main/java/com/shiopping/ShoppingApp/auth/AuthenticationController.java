f69e99a31d5a91795018b48fd0041314aa565f50
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-06 14:32:35 +0200

    Improve controller responses

--- src/main/java/com/shiopping/ShoppingApp/auth/AuthenticationController.java
+++ src/main/java/com/shiopping/ShoppingApp/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.shiopping.ShoppingApp.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authService;
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            return ResponseEntity.ok(authService.register(request));
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while registering new user: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        try {
+            return ResponseEntity.ok(authService.login(request));
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while logging user: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
